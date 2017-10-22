0aaf2fd1654701ff3c02c97f41f926612477f75e
VitorNunesPinto <vitornp@yahoo.com.br>
2017-10-22 19:54:10 -0200

    Criada as classes do model

---
+++ app/src/main/java/dcc196/trabalho_dcc/model/Participante.java
@@ -0,0 +1,51 @@
+package dcc196.trabalho_dcc.model;
+
+
+import java.util.Date;
+
+/**
+ * Created by Vitor on 22/10/2017.
+ */
+
+public class Participante {
+    private String nomeCompleto;
+    private String email;
+    private Date horaEntrada;
+    private Date horaSaída;
+
+    public Participante () {
+
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    public void setHoraEntrada(Date horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
+
+    public Date getHoraSaída() {
+        return horaSaída;
+    }
+
+    public void setHoraSaída(Date horaSaída) {
+        this.horaSaída = horaSaída;
+    }
+}
