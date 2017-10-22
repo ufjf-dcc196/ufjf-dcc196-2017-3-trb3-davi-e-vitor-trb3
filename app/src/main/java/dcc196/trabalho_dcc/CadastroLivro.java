6f1b01779eacbf843d0607c9720a6d9fca55805d
VitorNunesPinto <vitornp@yahoo.com.br>
2017-10-22 21:31:19 -0200

    Tela de cadastro de reserva atualizada

--- app/src/main/java/dcc196/trabalho_dcc/CadastroLivro.java
+++ app/src/main/java/dcc196/trabalho_dcc/CadastroLivro.java
@@ -2,12 +2,37 @@ package dcc196.trabalho_dcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import dcc196.trabalho_dcc.model.Livro;
 
 public class CadastroLivro extends AppCompatActivity {
+
+    private Button btnCadastrarLivro;
+    private EditText campoTitulo;
+    private EditText campoEditora;
+    private EditText campoAnoPubliicacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_livro);
+
+        btnCadastrarLivro = (Button) findViewById(R.id.btnSalvarLivro);
+        campoTitulo = (EditText) findViewById(R.id.txtCampoTitulo);
+        campoEditora = (EditText) findViewById(R.id.txtCampoEditora);
+        campoAnoPubliicacao = (EditText) findViewById(R.id.txtCampoAnoPublicacao);
+
+        btnCadastrarLivro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Livro livro = new Livro();
+                livro.setTitulo(campoTitulo.getText().toString());
+                livro.setEditora(campoEditora.getText().toString());
+                livro.setAnoPlubicacao(campoAnoPubliicacao.getText().toString());
+            }
+        });
     }
 }
--- app/src/main/java/dcc196/trabalho_dcc/CadastroReserva.java
+++ app/src/main/java/dcc196/trabalho_dcc/CadastroReserva.java
@@ -2,12 +2,48 @@ package dcc196.trabalho_dcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import dcc196.trabalho_dcc.model.Livro;
+import dcc196.trabalho_dcc.model.Participante;
+import dcc196.trabalho_dcc.model.Reserva;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class CadastroReserva extends AppCompatActivity {
+
+    private Button btnReservarLivro;
+    private AutoCompleteTextView autoCompleteParticipantes;
+    private AutoCompleteTextView autoCompleteLivros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_reserva);
+
+        btnReservarLivro = (Button) findViewById(R.id.btnReservarLivro);
+        autoCompleteParticipantes = (AutoCompleteTextView) findViewById(R.id.autoCompleteParticipantes);
+        autoCompleteLivros = (AutoCompleteTextView) findViewById(R.id.autoCompleteLivros);
+
+        final ArrayAdapter<Participante> adpParticipante = new ArrayAdapter<Participante>(this, R.layout.support_simple_spinner_dropdown_item);
+        autoCompleteParticipantes.setAdapter(adpParticipante);
+
+        final ArrayAdapter<Livro> adpLivro = new ArrayAdapter<Livro>(this, R.layout.support_simple_spinner_dropdown_item);
+        autoCompleteLivros.setAdapter(adpLivro);
+
+        btnReservarLivro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Reserva reserva = new Reserva();
+                reserva.setLivro(adpLivro.getItem(autoCompleteLivros.getListSelection()));
+                reserva.setParticipante(adpParticipante.getItem(autoCompleteParticipantes.getListSelection()));
+
+                Toast.makeText(getApplicationContext(), reserva.getLivro().getTitulo(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
