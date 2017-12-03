1cb95fea44e17b888cb479425ea05781cf67f8f1
VitorNunesPinto <vitornp@yahoo.com.br>
2017-12-03 19:44:41 -0200

    Construção da tela principal

--- app/src/main/java/dcc196/trabalho_dcc/MainActivity.java
+++ app/src/main/java/dcc196/trabalho_dcc/MainActivity.java
@@ -1,37 +1,30 @@
 package dcc196.trabalho_dcc;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-
-import dcc196.trabalho_dcc.model.Livro;
-import dcc196.trabalho_dcc.model.Participante;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnCadastrarParticipante;
-    private Button btnCadastrarReserva;
-    private Button btnCadastrarLivro;
-    private ListView lvParticipantes;
+    private Button btnCadastrarEtiquetas;
+    private Button btnCadastrarTarefas;
+    private Button btnVisualizarEtiquetas;
+    private Button btnVisualizarTarefas;
+    private ListView listview_main;
 
 
     @Override
     protected void onResume() {
         super.onResume();
-        BaseAdapter adapter = (BaseAdapter) lvParticipantes.getAdapter();
-        adapter.notifyDataSetChanged();
+        //BaseAdapter adapter = (BaseAdapter) listview_main.getAdapter();
+        //adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -40,110 +33,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCadastrarParticipante = (Button) findViewById(R.id.btnCadastrarParticipante);
-        btnCadastrarReserva = (Button) findViewById(R.id.btnCadastrarReserva);
-        btnCadastrarLivro = (Button) findViewById(R.id.btnCadastrarLivro);
-        lvParticipantes = (ListView) findViewById(R.id.lvParticipantes);
-
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        Date hora;  // Ou qualquer outra forma que tem
-        //String dataFormatada = sdf.format(hora);
-
-        ParticipanteHelper p = ParticipanteHelper.getInstance();
-        LivroHelper l = LivroHelper.getInstance();
-        if (p != null) {
-        //Carregamento de dados
-            Participante p1 = new Participante();
-            p1.setNomeCompleto("Davi de Almeida");
-            p1.setEmail("davi.c@gmail.com");
-            p.criar(p1);
-
-            Participante p2 = new Participante();
-            p2.setNomeCompleto("Ricardo Tuboly");
-            p2.setEmail("ricardo.c@gmail.com");
-            p.criar(p2);
-
-            if (l != null) {
-                Livro l1 = new Livro();
-                l1.setTitulo("Senhor dos Anéis");
-                l1.setEditora("Almenara");
-                l1.setAnoPlubicacao("2000");
-                l.criar(l1);
-
-                Livro l2 = new Livro();
-                l2.setTitulo("Star Wars");
-                l2.setEditora("Yoda");
-                l2.setAnoPlubicacao("1990");
-                l.criar(l2);
-            }
+        btnVisualizarEtiquetas = (Button) findViewById(R.id.btnVisualizarEtiquetas);
+        btnCadastrarEtiquetas = (Button) findViewById(R.id.btnCadastrarEtiquetas);
+        btnCadastrarTarefas = (Button) findViewById(R.id.btnCadastrarTarefas);
+        btnVisualizarTarefas = (Button) findViewById(R.id.btnVisualizarTarefas);
+        listview_main = (ListView) findViewById(R.id.listview_main);
 
 
-
-            ArrayAdapter<Participante> adapter = new ArrayAdapter<>(
-                    this, android.R.layout.simple_list_item_1, ParticipanteHelper.getInstance().listarParticipantes());
-
-            lvParticipantes.setAdapter(adapter);
-
-
-        }
-
-        btnCadastrarReserva.setOnClickListener(new View.OnClickListener() {
+        btnCadastrarTarefas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CadastroReserva.class);
-                startActivity(intent);
-            }
 
+            }
 
         });
 
-        btnCadastrarLivro.setOnClickListener(new View.OnClickListener() {
+        btnCadastrarEtiquetas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CadastroLivro.class);
-                startActivity(intent);
+
             }
         });
 
-        btnCadastrarParticipante.setOnClickListener(new View.OnClickListener() {
+        btnVisualizarTarefas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("TAG", String.valueOf(ParticipanteHelper.getInstance().listarParticipantes().size()));
-                Intent intent = new Intent(MainActivity.this, CadastroParticipante.class);
-                startActivity(intent);
+
             }
         });
 
-        lvParticipantes.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        btnVisualizarEtiquetas.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onClick(View v) {
 
-                Participante p = ParticipanteHelper.getInstance().listarParticipantes().get(position);
-
-                if (p.getHoraEntrada() == null) {
-                    p.setHoraEntrada(Calendar.getInstance().getTime());
-                    Toast.makeText(getApplicationContext(), "HoraEntrada atualizada", Toast.LENGTH_SHORT).show();
-                } else if (p.getHoraSaída() == null) {
-                    p.setHoraSaída(Calendar.getInstance().getTime());
-                    Toast.makeText(getApplicationContext(), "HoraSaida atualizada", Toast.LENGTH_SHORT).show();
-                } else {
-                    p.setHoraEntrada(null);
-                    p.setHoraSaída(null);
-                    Toast.makeText(getApplicationContext(), "Horas de entrada e saidas resetadas", Toast.LENGTH_SHORT).show();
-                }
-                return true;
-            }
-        });
-
-        lvParticipantes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-
-                Participante p = ParticipanteHelper.getInstance().listarParticipantes().get(position);
-                Intent intent = new Intent(MainActivity.this, DetalheParticipante.class);
-                intent.putExtra("Participante", p);
-                startActivity(intent);
             }
         });
     }
